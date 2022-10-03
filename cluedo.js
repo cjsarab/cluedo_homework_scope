591020fb9a00e8af5b599db053f909753032cfaa
Cammy Smith <cjsarab1991@gmail.com>
2022-10-03 16:18:09 +0100

    eight8

--- cluedo.js
+++ cluedo.js
@@ -125,3 +125,40 @@ const verdict = declareMurderer();
 console.log(verdict);
 
 // Mr Green will be the murderer because Mustard and Scarlett are local variables
+
+const scenario = {
+    murderer: 'Mrs. Peacock',
+    room: 'Conservatory',
+    weapon: 'Lead Pipe'
+};
+
+const changeScenario = function() {
+    scenario.murderer = 'Mrs. Peacock';
+    scenario.room = 'Dining Room';
+
+    const plotTwist = function(room) {
+    if (scenario.room === room) {
+        scenario.murderer = 'Colonel Mustard';
+    }
+
+    const unexpectedOutcome = function(murderer) {
+        if (scenario.murderer === murderer) {
+        scenario.weapon = 'Candle Stick';
+        }
+    }
+
+    unexpectedOutcome('Colonel Mustard');
+    }
+
+    plotTwist('Dining Room');
+}
+
+const declareWeapon = function() {
+    return `The weapon is ${scenario.weapon}.`
+}
+
+changeScenario();
+const verdict = declareWeapon();
+console.log(verdict);
+
+// Weapon = candlestick because the changescenario function calls plot twist before unexpected outcome, resulting in a truthy unexpected outcome
