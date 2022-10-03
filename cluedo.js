51ded062b47c7b1a267765f304a3819d1c3394bb
Cammy Smith <cjsarab1991@gmail.com>
2022-10-03 15:33:49 +0100

    First comment

--- cluedo.js
+++ cluedo.js
@@ -1,1 +1,14 @@
-start
+const scenario = {
+    murderer: 'Miss Scarlet',
+    room: 'Library',
+    weapon: 'Rope'
+    };
+
+const declareMurderer = function() {
+    return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// The output will show that the murderer is Miss Scarlett because she is the scenario.murderer
